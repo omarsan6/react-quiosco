7989fcc5bb4be9f6ec1eccddd40ba10a0975b66f
omarsanchez <omarcore@gmail.com>
2023-12-11 09:13:53 -0600

    Mostrando componente Layout en la pagina

--- src/router.jsx
+++ src/router.jsx
@@ -2,13 +2,10 @@ import {createBrowserRouter} from 'react-router-dom'
 import Layout from './layouts/Layout'
 
 
-const router = createBrowserRouter([
-
+export const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout/>
     }
-
 ])
 
-export default router
