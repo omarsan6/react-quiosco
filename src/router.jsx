0c1082e39d1caf4ea1b1634b59c41ab0a4258331
omarsanchez <omarcore@gmail.com>
2023-12-11 09:22:21 -0600

    Agregando el hijo/componente inicio  al Layout con Outlet

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,23 @@
 import {createBrowserRouter} from 'react-router-dom'
 import Layout from './layouts/Layout'
+import AuthLayout from './layouts/AuthLayout'
+import Inicio from './views/Inicio'
 
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Layout/>
-    }
+        element: <Layout/>,
+        children:[
+            {
+                index:true,
+                element: <Inicio/>
+            },
+        ]
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout />
+    },
 ])
 
