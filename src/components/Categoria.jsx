ed8062772d5d90d5c84021b0ec046bf33caf1c60
omarsanchez <omarcore@gmail.com>
2023-12-11 12:11:45 -0600

    Sidebar

---
+++ src/components/Categoria.jsx
@@ -0,0 +1,16 @@
+export default function Categoria({categoria}) {
+
+    const {icono, id, nombre} = categoria
+
+  return (
+    <div className="flex items-center gap-4 border w-full p-3 bg-amber-400 hover:bg-amber-600 cursor-pointer">
+      <img 
+      src={`../../public/img/icono_${icono}.svg`} 
+      alt="Imagen icono"
+      className="w-12" 
+      />
+
+      <p className="font-bold text-lg cursor-pointer truncate">{nombre}</p>
+    </div>
+  )
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,33 @@
+import { categorias } from "../data/categorias"
+import Categoria from "./Categoria"
+
 export default function Sidebar() {
-  return (
-    <div className="md:w-72">
-      Sidebar
-    </div>
-  )
+    return (
+        <aside className="md:w-72">
+            <div className="p-10 ">
+                <img
+                    src="../../public/img/logo.svg"
+                    alt="imagen logo"
+                    className="w-40" />
+            </div>
+
+            <div className="mt-2">
+                {categorias.map(categoria => (
+                    <Categoria 
+                        categoria={categoria}    
+                    />
+                ))}
+            </div>
+
+            <div className="mt-5 px-3">
+                <button
+                    type="button"
+                    className="text-center bg-red-500 hover:bg-red-700 w-full p-3 font-bold text-white"    
+                >
+                    Cancelar orden
+                </button>
+            </div>
+
+        </aside>
+    )
 }
